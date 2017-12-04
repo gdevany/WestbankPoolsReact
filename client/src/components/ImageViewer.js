4428939bc68975271bd44c209fc0b0fed9aae97d
Greg Devany <greg.devany@yahoo.com>
2017-12-04 10:58:04 -0600

    ImageViewer- react-image-gallery added, working 95% correct

--- client/src/components/ImageViewer.js
+++ client/src/components/ImageViewer.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import "react-image-gallery/styles/css/image-gallery.css";
+import ImageGallery from 'react-image-gallery';
+
 
 function ImageViewer(props) {
   // console.log(props);
-
-  var imageList = props.imageList;
   console.log(props.imageList);
   var viewIt = "";
+  var images = [];
+  props.imageList.map(i => {
+    return(
+    images = [...images, {original:require(`../pics/${i.url}`),
+      thumbnail:require(`../pics/${i.url}`)}]
+    )
+  })
 
-  if(props.projectChosen !== "" && imageList.length > 1) {
-    viewIt = <div onClick={() => props.setViewerList([])}>sdfasdfasdfasd</div>
+  if(props.projectChosen !== "" && props.imageList.length > 1) {
+    viewIt = (
+      <ImageGallery items={images} />
+    )
   } else viewIt = <div></div>
 
 
@@ -20,3 +30,25 @@ function ImageViewer(props) {
 }
 
 export default ImageViewer;
+
+
+// function ImageViewer(props) {
+//   // console.log(props);
+//
+//   var imageList = props.imageList;
+//   console.log(props.imageList);
+//   var viewIt = "";
+//
+//   if(props.projectChosen !== "" && imageList.length > 1) {
+//     viewIt = <div onClick={() => props.setViewerList([])}>sdfasdfasdfasd</div>
+//   } else viewIt = <div></div>
+//
+//
+//   return (
+//     <div>
+//     {viewIt}
+//   </div>
+//   )
+// }
+//
+// export default ImageViewer;
