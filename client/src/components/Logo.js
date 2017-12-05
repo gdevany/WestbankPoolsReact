c3b81f72bb30ea824e24ab095442055b1f6f0a32
Greg Devany <greg.devany@yahoo.com>
2017-12-05 12:54:53 -0600

    Detail- messing with the logo size

--- client/src/components/Logo.js
+++ client/src/components/Logo.js
@@ -2,10 +2,30 @@ import React from 'react';
 
 function Logo() {
   return(
-    <div className="seclogo">
-      <div className="logoinit"></div>
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-6 offset-sm-3 col-lg-8 offset-lg-2">
+          <div className="seclogo">
+            <div className="logoinit"></div>
+          </div>
+        </div>
+      </div>
     </div>
+
   )
 }
 
 export default Logo;
+
+
+// import React from 'react';
+//
+// function Logo() {
+//   return(
+//     <div className="seclogo">
+//       <div className="logoinit"></div>
+//     </div>
+//   )
+// }
+//
+// export default Logo;
